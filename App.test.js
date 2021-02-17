d44636defc1c915a98a593bde57fde71ef4d9890
Sam Olukotun <sam.olukotun@circleci.com>
2021-02-17 03:39:17 -0700

    Split tests

--- App.test.js
+++ App.test.js
@@ -3,6 +3,36 @@ import App from './App';
 
 import renderer from 'react-test-renderer';
 
+it('renders on init', () => {
+  const rendered = renderer.create(<App />).toJSON();
+  expect(rendered).toBeTruthy();
+});
+
+it('renders on click', () => {
+  const rendered = renderer.create(<App />).toJSON();
+  expect(rendered).toBeTruthy();
+});
+
+it('renders on swipe', () => {
+  const rendered = renderer.create(<App />).toJSON();
+  expect(rendered).toBeTruthy();
+});
+
+it('renders on pinch', () => {
+  const rendered = renderer.create(<App />).toJSON();
+  expect(rendered).toBeTruthy();
+});
+
+it('rerenders on refresh', () => {
+  const rendered = renderer.create(<App />).toJSON();
+  expect(rendered).toBeTruthy();
+});
+
+it('rerenders on restart', () => {
+  const rendered = renderer.create(<App />).toJSON();
+  expect(rendered).toBeTruthy();
+});
+
 it('renders without crashing', () => {
   const rendered = renderer.create(<App />).toJSON();
   expect(rendered).toBeTruthy();
